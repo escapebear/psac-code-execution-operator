f0cd84adce3db6b5818e5439f992c90ca4054a6d
fpjnne <fpjnne@inf.elte.hu>
2022-05-06 21:03:01 +0200

    minor fixes

--- test/watch_test.py
+++ test/watch_test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from kubernetes import client, config, watch
+import json
 
 
 def main():
@@ -8,7 +9,8 @@ def main():
 
     stream = watch.Watch().stream(crds.list_cluster_custom_object, 'operators.psac.inf.elte.hu', 'v1', 'codeexecutions', resource_version='')
     for event in stream:
-       print(event)
+        if event['raw_object']['metadata']['namespace'] == 'code-execution':
+           print(json.dumps(event['raw_object']['spec'], indent=4))
 
 
 if __name__ == '__main__':
